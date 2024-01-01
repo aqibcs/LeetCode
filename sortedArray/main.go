b761c7935983c90171a3a9188facc72429a77c52
aqibcs <aqibshahcs@gmail.com>
2024-01-01 11:13:34 +0500

    Sorted Array

--- sortedArray/main.go
+++ sortedArray/main.go
@@ -2,72 +2,23 @@ package main
 
 import (
 	"fmt"
-	"math"
+	"sort"
 )
 
 func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
-	// Ensure nums1 is the smaller array
-	if len(nums1) > len(nums2) {
-		nums1, nums2 = nums2, nums1
+	for i := 0; i < len(nums2); i++ {
+		nums1 = append(nums1, nums2[i])
 	}
 
-	x, y := len(nums1), len(nums2)
-	low, high := 0, x
-
-	for low <= high {
-		partitionX := (low + high) / 2
-		partitionY := (x+y+1)/2 - partitionX
-
-		maxLeftX := math.MinInt64
-		minRightX := math.MaxInt64
-
-		if partitionX > 0 {
-			maxLeftX = nums1[partitionX-1]
-		}
-
-		if partitionX < x {
-			minRightX = nums1[partitionX]
-		}
-
-		maxLeftY := math.MinInt64
-		minRightY := math.MaxInt64
-
-		if partitionY > 0 {
-			maxLeftY = nums2[partitionY-1]
-		}
-
-		if partitionY < y {
-			minRightY = nums2[partitionY]
-		}
-
-		if maxLeftX <= minRightY && maxLeftY <= minRightX {
-			if (x+y)%2 == 0 {
-				return float64(max(maxLeftX, maxLeftY)+min(minRightX, minRightY)) / 2.0
-			} else {
-				return float64(max(maxLeftX, maxLeftY))
-			}
-		} else if maxLeftX > minRightY {
-			high = partitionX - 1
-		} else {
-			low = partitionX + 1
-		}
+	sort.Slice(nums1, func(i, j int) bool {return nums1[i] < nums1[j]})
+	size := len(nums1)
+	if size%2 == 0 {
+		n1 := float64(nums1[size/2-1])
+		n2 := float64(nums1[size/2])
+		return (n1 + n2)/2
+	} else {
+		return float64(nums1[size/2])
 	}
-
-	panic("Input arrays are not sorted.")
-}
-
-func max(a, b int) int {
-	if a > b {
-		return a
-	}
-	return b
-}
-
-func min(a, b int) int {
-	if a < b {
-		return a
-	}
-	return b
 }
 
 func main() {
