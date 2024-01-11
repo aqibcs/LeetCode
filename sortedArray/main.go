4d25b64648d84b5430df264e74fd0f97e136dacf
aqibcs <aqibshahcs@gmail.com>
2024-01-11 14:49:22 +0500

    4. Median of Two Sorted Arrays

--- sortedArray/main.go
+++ sortedArray/main.go
@@ -10,12 +10,12 @@ func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
 		nums1 = append(nums1, nums2[i])
 	}
 
-	sort.Slice(nums1, func(i, j int) bool {return nums1[i] < nums1[j]})
+	sort.Slice(nums1, func(i, j int) bool { return nums1[i] < nums1[j] })
 	size := len(nums1)
 	if size%2 == 0 {
 		n1 := float64(nums1[size/2-1])
 		n2 := float64(nums1[size/2])
-		return (n1 + n2)/2
+		return (n1 + n2) / 2
 	} else {
 		return float64(nums1[size/2])
 	}
