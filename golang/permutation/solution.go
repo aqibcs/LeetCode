03f6d61e0035a282408186e32a3d196b370222ef
aqibcs <aqibshahcs@gmail.com>
2024-03-06 21:17:43 +0500

    46. Permutations

---
+++ golang/permutation/solution.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+func permute(nums []int) [][]int {
+	var result [][]int
+
+	var backtrack func(start int)
+	backtrack = func(start int) {
+		if start == len(nums)-1 {
+			currentPermutation := make([]int, len(nums))
+			copy(currentPermutation, nums)
+			result = append(result, currentPermutation)
+			return
+		}
+
+		for i := start; i < len(nums); i++ {
+			nums[start], nums[i] = nums[i], nums[start]
+			backtrack(start + 1)
+			nums[start], nums[i] = nums[i], nums[start]
+		}
+	}
+
+	backtrack(0)
+
+	return result
+}
+
+func main() {
+	nums1 := []int{1, 2, 3}
+	fmt.Println(permute(nums1))
+
+	nums2 := []int{0, 1}
+	fmt.Println(permute(nums2))
+
+	nums3 := []int{1}
+	fmt.Println(permute(nums3))
+}
